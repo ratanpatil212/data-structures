b090b6df2d5e14be717e38e3c14ffb709d35130f
ratanpatil212 <ratanpatil212@gmail.com>
2023-02-21 20:06:48 +0530

    graphs in matrxin n list

--- C++/graphs/grph-creation.cpp
+++ C++/graphs/grph-creation.cpp
@@ -1,59 +1,30 @@
 #include <iostream>
-#include <unordered_map>
-#include <list>
+#include <vector>
+
 using namespace std;
 
-class graph
-{
-public:
-    unordered_map<int, list<int>> adj;
+int main() {
+    int n,m;
+    cin >> n >> m;
+    // graph here
+    //adj matrix
+    // #costly
+    // int adj[n+1][m+1];
+    // for(int i =0;i<m;i++){
+    //     // O(n) space complexity
+    //     int u,v;
+    //     cin >> u >> v;
+    //     adj[u][v] = 1;
+    //     adj[v][u] =1;
+    // }
+    // return 0;
 
-    void addEdge(int u, int v, bool direction)
-    {
-        // direction = 0 -> undirected
-        // direction = 1 -> directed
-
-        // create an edge from u to v
+    //adj list
+    vector<int> adj[n+1];
+    for(int i =0;i<m;i++){
+        int u,v;
+        cin >> u>> v;
         adj[u].push_back(v);
-        if (direction == 0)
-        {
-            adj[v].push_back(u);
-        }
+        adj[v].push_back(u);
     }
-
-    void printAdjList()
-    {
-        for (auto i : adj)
-        {
-            cout << i.first << "->";
-            for (auto j : i.second)
-            {
-                cout << j << ", ";
-            }
-            cout << endl;
-        }
-    }
-};
-
-int main()
-{
-    int n;
-    cout << "Enter the number of nodes" << endl;
-    cin >> n;
-
-    int m;
-    cout << "Enter the number of edges " << endl;
-    cin >> m;
-    graph g;
-
-    for (int i = 0; i < m; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        // creating an undirected graph
-        g.addEdge(u, v, 0);
-    }
-
-    // print graph
-    g.printAdjList();
 }
